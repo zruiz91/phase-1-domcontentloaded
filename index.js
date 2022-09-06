6eaf009e5c8d10ce1fff0d5940f77c993bbe785d
Zach Ruiz <powersbooth10@gmail.com>
2022-09-06 11:10:51 -0500

    commit tree af3d73c903ecb73e1c6ca1858b69ffc1a2cbbecd

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    changeText()
+});
+
+const changeText = function() {
+    document.getElementById("text").innerHTML = "This is really cool!"
+}
+
+
+// document.addEventListener( "DOMContentLoaded", function () {
+//     updateDOM()
+// } );
+
+
+// function updateDOM() {
+//     document.getElementById( "text" )
+//     .innerHTML = "This is really cool!";
+// }
